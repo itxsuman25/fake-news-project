7db7edad8794a9d588d08f8062f58499f49e7a10
itxsuman25 <143999452+itxsuman25@users.noreply.github.com>
2025-06-25 17:33:46 +0530

    Update fake_news_detect_project.py

--- fake_news_detect_project.py
+++ fake_news_detect_project.py
@@ -10,7 +10,7 @@ Original file is located at
 from google.colab import drive
 drive.mount('/content/drive')
 
-!pip install transformers tqdm
+!pip install transformers
 
 # ✅ Optimized BERT Fake News Detection (Colab RAM Friendly Version)
 import pandas as pd
